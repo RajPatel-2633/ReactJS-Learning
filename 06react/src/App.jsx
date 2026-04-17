6e54d51cb7725629354a67ff727ce37bdc4c6844
Raj Patel <rajpatel.260303@gmail.com>
2026-04-17 22:05:37 +0530

    Learnt useState and useEffect Hooks

--- 06react/src/App.jsx
+++ 06react/src/App.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect} from "react";
 export function App(){
+    const [message,setMessage]=useState("Loading");
+    useEffect(()=>{
+        // This part is component did mount
+        
+           // return(This is component unmount);
+           fetch('https://api.freeapi.app/api/v1/public/dogs?page=1&limit=10&query=Affenpinscher')
+           .then((res)=>res.json())
+           .then((data)=> setMessage(data.message))
+           .catch(()=>setMessage("Failed to Fetch Data"));
+    },[])
+
     return (
         <div>
             <h1>Welcome to Our Restaurant</h1>
